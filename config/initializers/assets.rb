91574a2f6ff6eed4b3ed687a64bf23c836c9b048
Jean-Philippe MAGIDO <jean-philippemagido@Jean-Philippes-iMac.local>
2019-09-03 14:30:24 +0200

    try modif aos for heroku

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -14,3 +14,8 @@ Rails.application.config.assets.paths << Rails.root.join("vendor")
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+$(function() {
+  AOS.init();
+
+});
